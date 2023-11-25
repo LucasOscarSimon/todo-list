2c226bb011d4407d773a322e1799f571c91f378d
Lucas <lucas.oscar.simon@gmail.com>
2023-11-25 16:43:10 -0300

    Update tareasController spaces

--- src/controllers/tareasController.ts
+++ src/controllers/tareasController.ts
@@ -18,40 +18,43 @@ export const listarTareas = async (req: Request, res: Response) => {
   const tareas = tareasRaw.map(t => new Tarea(t.id, t.nombre, t.descripcion));
   
   res.render('tareas', { tareas });
-  };
+
+};
 
 
 export const agregarTarea = async (req: Request, res: Response) => {
-    try {
 
-        await tareaSchema.validateAsync(req.body);
-    
-        const { nombreTarea, descripcionTarea } = req.body;
-    
-        const nuevaTarea = new Tarea(null, nombreTarea, descripcionTarea);
-    
-        await prisma.tarea.create({
-          data: {
-            nombre: nuevaTarea.nombre,
-            descripcion: nuevaTarea.descripcion,
-          },
-        });
-    
-          // Redirige o renderiza según sea necesario
-          res.redirect('/tareas');
-      } catch (error) {
-        if (error instanceof Joi.ValidationError) {
-          // Manejar errores de validación
-          res.status(400).send(error.details.map(detail => detail.message).join(', '));
-        } else {
-          // Manejar otros errores
-          res.status(500).send('Error interno del servidor');
-        }
+  try {
+
+      await tareaSchema.validateAsync(req.body);
+  
+      const { nombreTarea, descripcionTarea } = req.body;
+  
+      const nuevaTarea = new Tarea(null, nombreTarea, descripcionTarea);
+  
+      await prisma.tarea.create({
+        data: {
+          nombre: nuevaTarea.nombre,
+          descripcion: nuevaTarea.descripcion,
+        },
+      });
+  
+        // Redirige o renderiza según sea necesario
+        res.redirect('/tareas');
+    } catch (error) {
+      if (error instanceof Joi.ValidationError) {
+        // Manejar errores de validación
+        res.status(400).send(error.details.map(detail => detail.message).join(', '));
+      } else {
+        // Manejar otros errores
+        res.status(500).send('Error interno del servidor');
       }
-  };
+    }
+};
 
 
-  export const borrarTarea = async (req: Request, res: Response) => {
+export const borrarTarea = async (req: Request, res: Response) => {
+
     const id = parseInt(req.params.id);
     let mensaje = '';
   
@@ -64,40 +67,43 @@ export const agregarTarea = async (req: Request, res: Response) => {
   
     const tareas = await prisma.tarea.findMany();
     res.render('tareas', { tareas, mensaje });
-  };
+
+};
   
 
 
-  export const actualizarTarea = async (req: Request, res: Response) => {
+export const actualizarTarea = async (req: Request, res: Response) => {
     
-    try {
+  try {
 
-        await tareaSchema.validateAsync(req.body);
-        const id = parseInt(req.params.id);
-        const { nombreTarea, descripcionTarea } = req.body;
-    
-        const tareaActualizada = new Tarea(id, nombreTarea, descripcionTarea);
-    
-        const tarea = await prisma.tarea.update({
-          where: { id },
-          data: {
-            nombre: tareaActualizada.nombre,
-            descripcion: tareaActualizada.descripcion,
-          },
-        });
-    
-        const tareas = await prisma.tarea.findMany();
-        res.render('tareas', { tareas });
-      } catch (error) {
-        if (error instanceof Joi.ValidationError) {
-          // Manejar errores de validación
-          res.status(400).send(error.details.map(detail => detail.message).join(', '));
-        } else {
-          // Manejar otros errores
-          res.status(500).send('Error interno del servidor');
-        }
+      await tareaSchema.validateAsync(req.body);
+      const id = parseInt(req.params.id);
+      const { nombreTarea, descripcionTarea } = req.body;
+  
+      const tareaActualizada = new Tarea(id, nombreTarea, descripcionTarea);
+  
+      const tarea = await prisma.tarea.update({
+        where: { id },
+        data: {
+          nombre: tareaActualizada.nombre,
+          descripcion: tareaActualizada.descripcion,
+        },
+      });
+  
+      const tareas = await prisma.tarea.findMany();
+      res.render('tareas', { tareas });
+    } catch (error) {
+      if (error instanceof Joi.ValidationError) {
+        // Manejar errores de validación
+        res.status(400).send(error.details.map(detail => detail.message).join(', '));
+      } else {
+        // Manejar otros errores
+        res.status(500).send('Error interno del servidor');
       }
-  };
+    }
+};
+
+
 async function tareaExists(id: number): Promise<boolean> {
     
     const tarea = await prisma.tarea.findFirst({
